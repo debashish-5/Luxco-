97e6ec3a7db8c577fa976354d1a0b47e578a1147
debashish-5 <ddebashishparida@gmail.com>
2026-04-24 23:45:24 +0530

    Added All Files

--- llm_chain.py
+++ llm_chain.py
@@ -3,8 +3,41 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableParallel,RunnableLambda
 from duckduckgo_search import DDGS
-
+from langchain.agents import create_react_agent, AgentExecutor
 from langchain.tools import tool
+from langchain import hub
+
+
+@tool
+def read_file(file_path:str) -> str:
+    """
+    Read the content of a file and return it as a string.
+    """
+    try:
+        with open(file_path, 'r',encoding='utf-8') as f:
+            return f.read()
+    except Exception as e:
+        return f"Error reading file: {e}"
+    
+@tool
+def save_notes(data:str, file_path:str) -> str:
+    """
+    Save the given data to a file at the specified path.
+    """
+    try:
+        with open(file_path, 'w',encoding='utf-8') as f:
+            f.write(data)
+        return f"Notes saved to {file_path}"
+    except Exception as e:
+        return f"Error saving notes: {e}"
+
+@tool 
+def current_time(dummy:str) -> str:
+    """
+    Return the current date and time as a string.
+    """
+    from datetime import datetime
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 @tool
 def farming_links(topic:str) -> str:
@@ -75,6 +108,24 @@ summary_chain = summary_prompt | model | parser
 example_chain = example_prompt | model | parser
 predict_chain = predict_prompt | model | parser
 tool_chain = tool_prompt | model | parser
+
+
+tools = [read_file,save_notes,current_time]
+#agent prompt
+agent_prompt = hub.pull("hwchase17/react")
+
+#create agent
+agent = create_react_agent(
+    llm=model,
+    tools=tools,
+    prompt = agent_prompt
+
+)
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    verbose=True
+)
 
 chains = RunnableParallel(
     explain=explain_chain,
